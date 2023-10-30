3ce981088f8cbae522389fd1fa86f5e743cd6b8c
anastasniu <nigit uanastasiia@gmail.com>
2023-10-30 21:20:36 +0600

    edited serializator.py

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from tours.models import Tour
 
 class ToursSerializer(serializers.ModelSerializer):
+    region = serializers.StringRelatedField()
     class Meta:
         model = Tour
         fields = '__all__'
